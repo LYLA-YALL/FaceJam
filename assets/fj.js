451be2a573bf3ba0fef84759c4748dc68074fdd6
unknown <lsplaver@yahoo.com>
2018-03-26 05:25:29 -0400

    require is now working correctly

--- assets/fj.js
+++ assets/fj.js
@@ -357,41 +357,59 @@ $(document).ready(function() {
     };
 
     //from spotifies web-api-auth-examples github
-    var request = require('request'); // "Request" library
+    // var request = require('request'); // "Request" library
+
+    var SpotifyWebApi = require('spotify-web-api-node');
 
     var client_id = 'CLIENT_ID'; // Your client id
     var client_secret = 'CLIENT_SECRET'; // Your secret
-    
-    // your application requests authorization
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      form: {
-        grant_type: 'client_credentials'
-      },
-      json: true
-    };
-    
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-    
-        // use the access token to access the Spotify Web API
-        var token = body.access_token;
-        var options = {
-          url: 'https://api.spotify.com/v1/users/jmperezperez',
-          headers: {
-            'Authorization': 'Bearer ' + token
-          },
-          json: true
-        };
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
-      }
+
+    var spotifyApi = new SpotifyWebApi({
+        client_id: 'f9193ba8699b4d488cf4ad4359844b04',
+        client_secret: 'e4804c0849cf403991e3cda12386b26b'
     });
     
+    // // your application requests authorization
+    // var authOptions = {
+    //   url: 'https://accounts.spotify.com/api/token',
+    //   headers: {
+    //     'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    //   },
+    //   form: {
+    //     grant_type: 'client_credentials'
+    //   },
+    //   json: true
+    // };
+    
+    // request.post(authOptions, function(error, response, body) {
+    //   if (!error && response.statusCode === 200) {
+    
+    //     // use the access token to access the Spotify Web API
+    //     var token = body.access_token;
+    //     var options = {
+    //       url: 'https://api.spotify.com/v1/users/jmperezperez',
+    //       headers: {
+    //         'Authorization': 'Bearer ' + token
+    //       },
+    //       json: true
+    //     };
+    //     request.get(options, function(error, response, body) {
+    //       console.log(body);
+    //     });
+    //   }
+    // });    
+
+    // Retrieve an access token.
+    spotifyApi.clientCredentialsGrant()
+      .then(function(data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+    
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+      }, function(err) {
+            console.log('Something went wrong when retrieving an access token', err);
+      });
 
     // $.ajax({
     //     url: 'https://accounts.spotify.com/api/token',
